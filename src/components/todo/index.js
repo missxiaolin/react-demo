078f34ec9d88ccc00bc5f9cd1d8e25ba818a9682
xiaolin <462441355@qq.com>
2018-03-31 21:38:38 +0800

    * 修改app.js 引入组件

---
+++ src/components/todo/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react'
+
+class Todo extends Component {
+    constructor(props) {
+        super(props)
+    }
+
+    render() {
+        return (
+            <p>this is todo components</p>
+        )
+    }
+}
+
+export default Todo
